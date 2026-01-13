8147df9c118b3d3401b80c0cddc7151f32f383c2
Jesus Altuve <jaltuve@gmail.com>
2026-01-13 10:36:47 -0500

    Misc updates

--- src/LABELS/LABEL_SET_CMWS_DISPLAY/LEDMapping.h
+++ src/LABELS/LABEL_SET_CMWS_DISPLAY/LEDMapping.h
@@ -3,12 +3,12 @@
 
 // Embedded LEDMapping structure and enums
 enum LEDDeviceType {
-  DEVICE_PCA9555,
-  DEVICE_GPIO,
   DEVICE_TM1637,
+  DEVICE_GPIO,
   DEVICE_NONE,
-  DEVICE_GN1640T,
+  DEVICE_PCA9555,
   DEVICE_WS2812,
+  DEVICE_GN1640T,
   DEVICE_GAUGE,
 };
 
--- src/LABELS/active_set.h
+++ src/LABELS/active_set.h
@@ -1,5 +1,5 @@
-// PID:0x1BAC 
+// PID:0xC1E5 
 
 #pragma once
 
-#define LABEL_SET TEST_ONLY
+#define LABEL_SET CMWS_DISPLAY
