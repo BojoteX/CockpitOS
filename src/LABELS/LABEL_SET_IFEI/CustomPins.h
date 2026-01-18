ee732ec03dc661b5b900dbed8a5fb7e2772527a0
Jesus Altuve <jaltuve@gmail.com>
2026-01-18 08:39:43 -0500

    Renamed Python Tools

--- src/LABELS/LABEL_SET_IFEI/CustomPins.h
+++ src/LABELS/LABEL_SET_IFEI/CustomPins.h
@@ -2,6 +2,10 @@
 
 #pragma once
 
+// --- Feature Enables ---
+#define ENABLE_TFT_GAUGES                         0 // Enable TFT Gauges for this specific LABEL SET? 1 = Yes (Enable only if TFT Gauges are present in your hardware/PCB)
+#define ENABLE_PCA9555                            0 // Should we enable PCA9555 logic to this LABEL SET? 1 = Yes (Enable only if PCA expanders are present in your hardware/PCB)
+
 // --- PINs for Jettison Select Panel with IFEI Panel ---
 #define JETT_DIO_PIN                          PIN(8)
 #define JETT_CLK_PIN                          PIN(9)
--- src/LABELS/LABEL_SET_MAIN/CustomPins.h
+++ src/LABELS/LABEL_SET_MAIN/CustomPins.h
@@ -2,6 +2,10 @@
 
 #pragma once
 
+// --- Feature Enables ---
+#define ENABLE_TFT_GAUGES                         0 // Enable TFT Gauges for this specific LABEL SET? 1 = Yes (Enable only if TFT Gauges are present in your hardware/PCB)
+#define ENABLE_PCA9555                            1 // Should we enable PCA9555 logic to this LABEL SET? 1 = Yes (Enable only if PCA expanders are present in your hardware/PCB)
+
 // I2C Pins
 #define SDA_PIN                               PIN(8)
 #define SCL_PIN                               PIN(9)
