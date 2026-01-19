c51534845331ca5a8214768f3bb486555396b92e
Jesus Altuve <136034141+BojoteX@users.noreply.github.com>
2026-01-19 17:33:18 -0500

    Moved bootloader functions outside RingBuffer

--- src/LABELS/LABEL_SET_TEST_ONLY/LEDMapping.h
+++ src/LABELS/LABEL_SET_TEST_ONLY/LEDMapping.h
@@ -4,12 +4,12 @@
 // Embedded LEDMapping structure and enums
 enum LEDDeviceType {
   DEVICE_NONE,
-  DEVICE_WS2812,
-  DEVICE_GPIO,
-  DEVICE_GN1640T,
-  DEVICE_GAUGE,
   DEVICE_TM1637,
+  DEVICE_GPIO,
   DEVICE_PCA9555,
+  DEVICE_GN1640T,
+  DEVICE_WS2812,
+  DEVICE_GAUGE,
 };
 
 struct LEDMapping {
