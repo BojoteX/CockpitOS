d359dd4c946748d6f4cd66a3fc309a56cdad0bcc
Jesus Altuve <jaltuve@gmail.com>
2026-02-27 10:23:14 -0500

    Clean up temp files

--- src/LABELS/LABEL_SET_RS485_LOLIN_AUTO/LabelSetConfig.h
+++ src/LABELS/LABEL_SET_RS485_LOLIN_AUTO/LabelSetConfig.h
@@ -4,6 +4,6 @@
 
 #define LABEL_SET_NAME        "RS485_LOLIN_AUTO"
 #define HAS_HID_MODE_SELECTOR 0
-#define LABEL_SET_FULLNAME    "RS485 Lolin" // You can safely change this
+#define LABEL_SET_FULLNAME    "Lolin TEST RS485" // You can safely change this
 #define HAS_RS485_LOLIN_AUTO
-#define AUTOGEN_USB_PID       0x3B38 // DO NOT EDIT THIS
+#define AUTOGEN_USB_PID       0x4A9A // DO NOT EDIT THIS
