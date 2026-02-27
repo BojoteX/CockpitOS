52d0ae8a14906bc51644477eee0b41e9db37b4bc
Jesus Altuve <jaltuve@gmail.com>
2026-02-27 08:46:40 -0500

    Custom Pin editor changes

--- src/LABELS/LABEL_SET_CMWS_DISPLAY/CustomPins.h
+++ src/LABELS/LABEL_SET_CMWS_DISPLAY/CustomPins.h
@@ -1,7 +1,20 @@
-// CustomPins.h - Configuration PINS for Apache CMWS LABEL SET
+// CustomPins.h - Pin configuration for CMWS_DISPLAY
+//
+// Managed by the Label Creator Custom Pins editor.
+// Unknown defines are preserved at the bottom of this file.
 
 #pragma once
 
-// --- Feature Enables ---
-#define ENABLE_TFT_GAUGES                         1 // Enable TFT Gauges for this specific LABEL SET? 1 = Yes (Enable only if TFT Gauges are present in your hardware/PCB)
-#define ENABLE_PCA9555                            0 // Should we enable PCA9555 logic to this LABEL SET? 1 = Yes (Enable only if PCA expanders are present in your hardware/PCB)
+// --- Feature Enables --------------------------------------
+#define ENABLE_TFT_GAUGES                        1              // Enable TFT gauge rendering (only if TFT displays are present)
+#define ENABLE_PCA9555                           0              // Enable PCA9555 I/O expander logic (only if PCA chips are present)
+
+// --- RS485 Communication ----------------------------------
+#define RS485_TX_PIN                             17             // GPIO for TX (connect to RS485 board DI)
+#define RS485_RX_PIN                             18             // GPIO for RX (connect to RS485 board RO)
+#define RS485_DE_PIN                             -1             // Direction enable pin (-1 if auto-direction)
+
+// --- RS485 Test Pins --------------------------------------
+#define RS485_TEST_BUTTON_GPIO                   0              // Test button GPIO (-1 to disable)
+#define RS485_TEST_SWITCH_GPIO                   -1             // Test switch GPIO (-1 to disable)
+#define RS485_TEST_LED_GPIO                      -1             // Test LED GPIO (-1 to disable)
--- src/LABELS/active_set.h
+++ src/LABELS/active_set.h
@@ -1,5 +1,5 @@
-// PID:0x589E 
+// PID:0x3B38 
 
 #pragma once
 
-#define LABEL_SET ALR67
+#define LABEL_SET RS485_LOLIN_AUTO
