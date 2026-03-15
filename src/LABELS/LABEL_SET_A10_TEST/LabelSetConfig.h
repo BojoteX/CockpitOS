798a79364176ae2ed85d321e88d4d2f792f3e0f0
Jesus Altuve <136034141+BojoteX@users.noreply.github.com>
2026-03-14 22:14:47 -0400

    Misc Changes

--- src/LABELS/LABEL_SET_A10_TEST/LabelSetConfig.h
+++
@@ -1,9 +0,0 @@
-#if __has_include("./CustomPins.h")
-#include "./CustomPins.h"
-#endif
-
-#define LABEL_SET_NAME        "A10_TEST"
-#define HAS_HID_MODE_SELECTOR 0
-#define LABEL_SET_FULLNAME    "CockpitOS Panel A10_TEST" // You can safely change this
-#define HAS_A10_TEST
-#define AUTOGEN_USB_PID       0x9AA1 // DO NOT EDIT THIS
--- src/LABELS/LABEL_SET_F18_HUD_PANEL_LIGHT_CENCIO/LabelSetConfig.h
+++
@@ -1,9 +0,0 @@
-#if __has_include("./CustomPins.h")
-#include "./CustomPins.h"
-#endif
-
-#define LABEL_SET_NAME        "F18_HUD_PANEL_LIGHT_CENCIO"
-#define HAS_HID_MODE_SELECTOR 0
-#define LABEL_SET_FULLNAME    "BLE Test for Cencio" // You can safely change this
-#define HAS_F18_HUD_PANEL_LIGHT_CENCIO
-#define AUTOGEN_USB_PID       0x9470 // DO NOT EDIT THIS
