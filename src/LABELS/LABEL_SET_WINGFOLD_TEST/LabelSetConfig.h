85f07b534f43d4cbf03dc2caaebb58684f38d973
Jesus Altuve <jaltuve@gmail.com>
2026-03-21 13:28:43 -0400

    WiFI DEBUG Ringbuffer set to 0

---
+++ src/LABELS/LABEL_SET_WINGFOLD_TEST/LabelSetConfig.h
@@ -0,0 +1,9 @@
+#if __has_include("./CustomPins.h")
+#include "./CustomPins.h"
+#endif
+
+#define LABEL_SET_NAME        "WINGFOLD_TEST"
+#define HAS_HID_MODE_SELECTOR 0
+#define LABEL_SET_FULLNAME    "CockpitOS Panel WINGFOLD_TEST" // You can safely change this
+#define HAS_WINGFOLD_TEST
+#define AUTOGEN_USB_PID       0xB12A // DO NOT EDIT THIS
